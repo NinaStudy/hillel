d8dc5bf02bed51c2e3dc9292faf19f1ae542e5f2
Nina <ninacherevko21@gmail.com>
2024-12-09 15:15:13 +0200

    ДЗ 11. MySql, Insert

--- index.php
+++ index.php
@@ -3,16 +3,10 @@ require_once "vendor/autoload.php";
 $config = require "Config/controller.php";
 
 use App\Core\Router;
-use App\Orm\Connector;
 
-try {
-    $router = new Router($config);
-    $router->run();
-}catch (\Throwable $e){
-    print 'message '.$e->getMessage();
-    print 'file '.$e->getFile();
-    print 'line '.$e->getLine();
-}
+$router = new Router($config);
+$router->run();
+
 
 
 
